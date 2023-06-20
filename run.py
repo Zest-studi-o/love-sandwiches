971438f94fccc2b67480407b5bb6a47440847184
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-06-20 20:10:35 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -34,8 +34,8 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there are not exactly 6 values.
     """
-
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
